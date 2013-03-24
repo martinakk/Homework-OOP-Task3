8e190602614665e867085aaa09b17a564562b1b4
martinakk <scourge89@gmail.com>
2013-03-24 22:32:46 +0200

    Update TestAnimal.cs

--- TestAnimal.cs
+++ TestAnimal.cs
@@ -9,71 +9,43 @@ namespace AnimalHierarchy
     class TestAnimal
     {
 
-        public static void AverageAge(List<Kitten> animals)
+        public static void AverageAge(List<Animal> animals)
         {
             byte sum = 0;
+            string kind = "";
             foreach (var animal in animals)
             {
                 sum += animal.Age;
+                kind = animal.GetType().ToString();
             }
             float average = sum / 2;
-            Console.WriteLine("The average age of all kittens is {0}", average); 
-        }
-        public static void AverageAge(List<Tomcat> animals)
-        {
-            byte sum = 0;
-            foreach (var animal in animals)
-            {
-                sum += animal.Age;
-            }
-            float average = sum / 2;
-            Console.WriteLine("The average age of all tomcats is {0}", average);
-        }
-        public static void AverageAge(List<Dog> animals)
-        {
-            byte sum = 0;
-            foreach (var animal in animals)
-            {
-                sum += animal.Age;
-            }
-            float average = sum / 2;
-            Console.WriteLine("The average age of all dogs is {0}", average);
-        }
-        public static void AverageAge(List<Frog> animals)
-        {
-            byte sum = 0;
-            foreach (var animal in animals)
-            {
-                sum += animal.Age;
-            }
-            float average = sum / 2;
-            Console.WriteLine("The average age of all frogs is {0}", average);
+            Console.WriteLine("The average age of all {0}s is {1}", kind, average); 
         }
 
         static void Main()
         {
-            List<Kitten> kittens = new List<Kitten>();
+            List<Animal> kittens = new List<Animal>();
             kittens.Add(new Kitten("Shushi", 1));
             kittens.Add(new Kitten("Linda", 2));
             kittens.Add(new Kitten("Miki", 5));
 
             AverageAge(kittens);
 
-            List<Tomcat> tomcats = new List<Tomcat>();
+            List<Animal> tomcats = new List<Animal>();
             tomcats.Add(new Tomcat("Shush", 2));
             tomcats.Add(new Tomcat("Joro", 3));
             tomcats.Add(new Tomcat("Miki", 6));
 
             AverageAge(tomcats);
 
-            List<Dog> dogs = new List<Dog>();
+            List<Animal> dogs = new List<Animal>();
             dogs.Add(new Dog("Shusho", "male",8,"breeed"));
             dogs.Add(new Dog("Joro", "male",4,"breeeeeed"));
             dogs.Add(new Dog("Miki", "female",2,"breeeed"));
 
             AverageAge(dogs);
 
-            List<Frog> frogs = new List<Frog>();
+            List<Animal> frogs = new List<Animal>();
             frogs.Add(new Frog("Shusho", "male", 1, "pink"));
             frogs.Add(new Frog("Joro", "male", 2, "green"));
             frogs.Add(new Frog("Miki", "female", 3, "purple"));
